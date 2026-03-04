feea59170d175de411faf669a52e6a2fd97e1e3d
Sara Zakarya <sarazakarya8@gmail.com>
2026-03-05 00:04:45 +0200

    feat: add routing configuration and structure

---
+++ src/Routes/route.tsx
@@ -0,0 +1,35 @@
+import { Login } from "@/pages/Login";
+import PasswordRecovery from "@/pages/PasswordRecovery";
+import { PasswordRecoveryWithPhone } from "@/pages/PasswordRecoveryWithPhone";
+import { Register } from "@/pages/Register";
+import { ResendPassword } from "@/pages/ResendPassword";
+import VerificationCode from "@/pages/VerificationCode";
+
+import { createBrowserRouter } from "react-router-dom";
+
+export const route = createBrowserRouter([
+  {
+    path: "Register",
+    element: <Register />,
+  },
+  {
+    path: "Login",
+    element: <Login />,
+  },
+  {
+    path: "PasswordRecovery",
+    element: <PasswordRecovery />,
+  },
+  {
+    path: "VerificationCode",
+    element: <VerificationCode />,
+  },
+  {
+    path: "ResendPassword",
+    element: <ResendPassword />,
+  },
+  {
+    path: "PasswordRecoveryWithPhone",
+    element: <PasswordRecoveryWithPhone />,
+  },
+]);
