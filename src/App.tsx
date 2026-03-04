39ad8a0beb8e6721ab585f9822221752818e0498
Sara Zakarya <sarazakarya8@gmail.com>
2026-03-05 01:34:56 +0200

    save current changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+=======
+import { RouterProvider } from "react-router-dom";
+import { route } from "./Routes/route";
+>>>>>>> parent of 1f44f03 (Merge branch 'feature/register' into dev)
 
 const App = () => {
   return (
