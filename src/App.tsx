38dda3ab08fbc2bcad90fd72bd5013e98ab73a45
Sara Zakarya <sarazakarya8@gmail.com>
2026-03-05 00:19:50 +0200

    fix: update App.tsx to use RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Register } from "./pages/Register";
-import { Login } from "./pages/Login";
-import PasswordRecovery from "./pages/PasswordRecovery";
-import VerificationCode from "./pages/VerificationCode";
-import { ResendPassword } from "./pages/ResendPassword";
-import { PasswordRecoveryWithPhone } from "./pages/PasswordRecoveryWithPhone";
-
-const router = createBrowserRouter([
-  {
-    path: "Register",
-    element: <Register />,
-  },
-  {
-    path: "Login",
-    element: <Login />,
-  },
-  {
-    path: "PasswordRecovery",
-    element: <PasswordRecovery />,
-  },
-  {
-    path: "VerificationCode",
-    element: <VerificationCode />,
-  },
-  {
-    path: "ResendPassword",
-    element: <ResendPassword />,
-  },
-  {
-    path: "PasswordRecoveryWithPhone",
-    element: <PasswordRecoveryWithPhone />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { route } from "./Routes/route";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={route} />;
 };
 
 export default App;
