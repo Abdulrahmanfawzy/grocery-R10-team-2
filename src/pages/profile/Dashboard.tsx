df7be331e092c7944dee29d4a5b4615427bab0e7
Ibrahim Kadry <100775114+Ebrahimkadry22@users.noreply.github.com>
2026-03-06 22:15:09 +0200

    finish one link dashboard pageprofile

--- src/pages/profile/Dashboard.tsx
+++ src/pages/profile/Dashboard.tsx
@@ -1,11 +1,15 @@
 import DashboardWelcome from "@/components/dashboard/DashboardWelcome"
 import OrderSection from "@/components/dashboard/OrderSection"
+import ShoppingInsights from "@/components/dashboard/ShoppingInsights"
+import TopPurchases from "@/components/dashboard/TopPurchases"
 
 const Dashboard = () => {
   return (
     <div>
       <DashboardWelcome />
       <OrderSection />
+      <ShoppingInsights />
+      <TopPurchases />
     </div>
   )
 }
